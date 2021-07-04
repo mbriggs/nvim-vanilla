6adfe57c3e54c788c72df394c70f80ed690c7474
Matt Briggs <matt@mattbriggs.net>
2021-07-04 11:10:29 -0400

    simplify dap

--- lua/mb/p/dap.lua
+++ lua/mb/p/dap.lua
@@ -1,7 +1,7 @@
 local dap = require'dap'
-local ui = require'dapui'
+-- local ui = require'dapui'
 local k = vim.keymap
-local g = vim.g
+-- local g = vim.g
 
 k.nnoremap{ '<leader>dd', function() dap.toggle_breakpoint() end }
 k.nnoremap{ '<leader>db', function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end }
@@ -12,19 +12,19 @@ k.nnoremap{ '<leader>dR', function() dap.run_last() end }
 k.nnoremap{ '<c-j>', function() dap.step_over() end }
 k.nnoremap{ '<c-k>', function() dap.step_into() end }
 k.nnoremap{ '<c-l>', function() dap.step_out() end }
-
+k.nnoremap{ '<leader>dk', function() require("dap.ui.variables").hover() end }
 -- ui
 
-k.nnoremap{ '<leader>du', function() ui.toggle() end }
+-- k.nnoremap{ '<leader>du', function() ui.toggle() end }
 
-g.dap_virtual_text = true
+-- g.dap_virtual_text = true
 dap.set_log_level("DEBUG")
 
 dap.adapters.go = function(callback, config)
     local handle
     local pid_or_err
     local port = 38697
-    handle, pid_or_err =
+    --[[ handle, pid_or_err =
       vim.loop.spawn(
       "dlv",
       {
@@ -42,26 +42,26 @@ dap.adapters.go = function(callback, config)
         --dap.repl.open()
         callback({type = "server", host = "127.0.0.1", port = port})
       end,
-      100)
+      100) ]]
 
 
-    -- callback({type = "server", host = "127.0.0.1", port = port})
-  end
-  -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
-  dap.configurations.go = {
-    {
-      type = "go",
-      name = "Debug",
-      request = "launch",
-      program = "./${relativeFileDirname}"
-    },
-    {
-      type = "go",
-      name = "Debug test", -- configuration for debugging test files
-      request = "launch",
-      mode = "test",
-      program = "./${relativeFileDirname}"
-    },
+    callback({type = "server", host = "127.0.0.1", port = port})
+end
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+dap.configurations.go = {
+  {
+    type = "go",
+    name = "Debug",
+    request = "launch",
+    program = "./${relativeFileDirname}"
+  },
+  {
+    type = "go",
+    name = "Debug test", -- configuration for debugging test files
+    request = "launch",
+    mode = "test",
+    program = "./${relativeFileDirname}"
+  },
 }
 
 -- ruby
@@ -83,4 +83,4 @@ dap.configurations.ruby = {
   },
 }
 
-ui.setup()
+-- ui.setup()
