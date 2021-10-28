476dbe0a5a1b1be552f8248a7f368d5f5ed03354
Matt Briggs <matt@mattbriggs.net>
2021-10-28 07:22:50 -0700

    update autopairs

--- lua/mb/p/nvim-tree.lua
+++ lua/mb/p/nvim-tree.lua
@@ -4,9 +4,9 @@ g.nvim_tree_width = '20%'
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_group_empty = 1
-g.nvim_tree_lsp_diagnostics = 1
 
 require('nvim-tree').setup {
+  nvim_tree_lsp_diagnostics = true,
   open_on_setup = true,
   hijack_cursor = true,
   view = {
