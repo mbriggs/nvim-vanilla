8c9d9cda28a5070600bf571c1cc96f01901be327
Matt Briggs <matt@mattbriggs.net>
2021-10-28 07:21:53 -0700

    update autopair

--- lua/mb/p/autopairs.lua
+++ lua/mb/p/autopairs.lua
@@ -5,7 +5,7 @@ ap.add_rules(require('nvim-autopairs.rules.endwise-elixir'))
 ap.add_rules(require('nvim-autopairs.rules.endwise-lua'))
 ap.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
 
-require("nvim-autopairs.completion.cmp").setup({
+require("nvim-autopairs").setup({
   map_cr = true, --  map <CR> on insert mode
   map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
   auto_select = true, -- automatically select the first item
