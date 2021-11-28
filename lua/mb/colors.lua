da0c7e7d0e344c10e6c5a129149b9c60e541d966
Matt Briggs <matt@mattbriggs.net>
2021-11-28 11:55:17 -0800

    highlight lspkind icons by type

--- lua/mb/colors.lua
+++ lua/mb/colors.lua
@@ -5,6 +5,25 @@ function P.config()
   local o = vim.o
   local g = vim.g
 
+  cmd([[
+" nvim-cmp visual studio code dark+ colors 
+
+" gray
+highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
+" blue
+highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
+highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
+" light blue
+highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
+highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
+highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
+" pink
+highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
+highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
+" front
+highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
+  ]])
+
   cmd('let &t_8f = "\\<Esc>[38;2;%lu;%lu;%lum"')
   cmd('let &t_8b = "\\<Esc>[48;2;%lu;%lu;%lum"')
 
